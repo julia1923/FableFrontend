66ddcea6cd431dbb835e6265c0119edd6f07af1c
julia1923 <juliaf.nascimento65@gmail.com>
2024-12-02 10:58:27 +0000

    addd login request

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,6 +4,8 @@ import styles from './style.module.css';
 import verifyLogin from '../../utils/Auth';
 import { useNavigate } from 'react-router-dom';
 import { getCookie } from '../../utils/cookieUtils';
+import axios from "axios";
+
 
 const Login = () => {
   const [message, setMessage] = useState<string | null>(null);
@@ -24,16 +26,24 @@ const Login = () => {
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
 
-    const matchedUser = verifyLogin(email, password);
-
-    if (matchedUser) {
-      document.cookie = `fable-auth-v.1.0.0=${encodeURIComponent(
-        JSON.stringify(matchedUser)
-      )}; path=/; max-age=3600`; 
-      navigate('/home')
-    } else {
-      setMessage('E-mail ou senha inválidos.');
-    }
+    const options = {
+      method: 'POST',
+      url: 'https://expert-barnacle-7v7gqv5pjwx43pq44-8080.app.github.dev/auth',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: {email: email, password: password}
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+        document.cookie = `fable-auth-v.1.0.0=${encodeURIComponent(
+          JSON.stringify(response.data.Bearer)
+        )}; path=/; max-age=3600`; 
+        navigate('/home')
+    }).catch(function (error) {
+        setMessage('E-mail ou senha inválidos.')
+    });
   };
 
   return(
